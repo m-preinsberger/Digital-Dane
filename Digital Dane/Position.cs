85176fa8135d47392cbef01b90fb618ee2f6575e
Jonas Schwarzbauer <20212020@htl-neufelden.at>
2025-02-24 18:36:23 +0100

    position

---
+++ Digital Dane/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Dane
+{
+    public record Position(int? Left, int? Top)
+    {
+        // Manhattan Distance Method
+        public int ManhattanDistance(Position other)
+        {
+            if (Left is null || Top is null || other.Left is null || other.Top is null)
+                throw new InvalidOperationException("Both positions must have non-null coordinates.");
+
+            return Math.Abs(Left.Value - other.Left.Value) + Math.Abs(Top.Value - other.Top.Value);
+        }
+    }
+}
