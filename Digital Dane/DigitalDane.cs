7edc8bf707e1c3aab70c18245c72b6fcc71e50a9
Jonas Schwarzbauer <20212020@htl-neufelden.at>
2025-02-24 18:21:34 +0100

    bot class created

---
+++ Digital Dane/DigitalDane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Dane
+{
+    internal class DigitalDane
+    {
+        public void Start()
+        {
+            // 1. look/scan
+            // 2. aim at nearest enemy
+        }
+    }
+}
